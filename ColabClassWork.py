abc13c92932b432f6d458f80b7d9f2ceec6448cc
Sebasgrra <sebasgrra@gmail.com>
2023-11-26 12:43:04 -0500

    Trabajo colab desempeño

---
+++ ColabClassWork.py
@@ -0,0 +1,3 @@
+Link: https://colab.research.google.com/drive/1HCFc5Q1vcaFqrdpTIwDh6W5eLclpK28j?usp=sharing
+Link: https://colab.research.google.com/drive/1H9bplnUocNU97nFhh180uFVXldfHqTCU?usp=sharing
+Link: https://colab.research.google.com/drive/1h0ez-V3zrefFQrHYUPonN-6wRwL2nPj9?usp=sharing
